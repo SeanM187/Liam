89e456b80c567c5b40c3426b4ccd515a65284cf1
SeanM187 <125615741+SeanM187@users.noreply.github.com>
2024-02-13 13:11:07 +1300

    added help and history button

--- C1_converter_gui_v1.py
+++ C1_converter_gui_v1.py
@@ -7,8 +7,8 @@ class Converter:
 
         # common format for all buttons
         # arial size 14 bold, with white text
-        button_font = ("Arial", "14", "bold")
-        button_fg = "FFFFFF"
+        button_font = ("Arial", "11", "bold")
+        button_fg = "#FFFFFF"
 
         # set up gui frame
         self.temp_frame = Frame(padx=10, pady=10)
@@ -33,13 +33,27 @@ class Converter:
         self.button_frame = Frame(self.temp_frame)
         self.button_frame.grid(row=4)
 
+        # to celcius
         self.to_celsius_button = Button(self.button_frame, text="To DegreesC", bg="#990099",
-                                        fg=button_fg, font=button_font)
+                                        fg=button_fg, font=button_font, width=12)
         self.to_celsius_button.grid(row=0, column=0)
 
+        # to farenheit
         self.to_farenheit_button = Button(self.button_frame, text="To Farenheit", bg="#009900",
-                                          fg=button_fg, font=button_font)
+                                          fg=button_fg, font=button_font, width=12)
         self.to_farenheit_button.grid(row=0, column=1)
+
+        # get help / info
+        self.get_help_button = Button(self.button_frame, text="Help / Info", bg="#ff8c00",
+                                          fg=button_fg, font=button_font, width=12)
+        self.get_help_button.grid(row=1, column=0, padx=5, pady=5)
+
+        # get history / export
+        self.to_history_export_button = Button(self.button_frame, text="History / Export", bg="#0d00ff",
+                                               fg=button_fg, font=button_font, width=12, state=DISABLED)
+        self.to_history_export_button.grid(row=1, column=1, padx=5, pady=5)
+
+
 
 # main routine
 
